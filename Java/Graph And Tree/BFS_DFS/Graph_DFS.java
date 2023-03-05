d9bd6382b7a48a4364f7583ff6c69eefeb5b6c57
Pankaj Mahanto <aryanpankaj78@gmail.com>
2023-03-05 17:10:09 +0600

    TC

--- Java/Graph And Tree/BFS_DFS/Graph_DFS.java
+++ Java/Graph And Tree/BFS_DFS/Graph_DFS.java
@@ -1,59 +1,76 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-class Graph{
-   private int Vertex;
-   private ArrayList<Character>v;
-   private boolean [][] Arr;
-    public Graph(int Vertex){
-       this.Vertex=Vertex;
-       this.v=new ArrayList<Character>();
-       this.Arr=new boolean[Vertex][Vertex];
-    }
-    public void addVertex(char n){
-         v.add(n);
-    }
-    public void addEdge(char s,char d){
-       int i=v.indexOf(s);
-       int j=v.indexOf(d);
-       Arr[i][j]=true;
-       Arr[j][i]=true;
+
+class Graph {
+    private int Vertex;
+    private ArrayList<Character> v;
+    private boolean[][] Arr;
+
+    public Graph(int Vertex) {
+        this.Vertex = Vertex;
+        this.v = new ArrayList<Character>();
+        this.Arr = new boolean[Vertex][Vertex];
     }
 
-    public void DFS(char s){
-     boolean [] visited=new boolean[Vertex];
-     int startI=v.indexOf(s);
+    public void addVertex(char n) {
+        v.add(n);
+    }
+
+    public void addEdge(char s, char d) {
+        int i = v.indexOf(s);
+        int j = v.indexOf(d);
+        Arr[i][j] = true;
+        Arr[j][i] = true;
+    }
+
+    public void DFS(char s) {
+        boolean[] visited = new boolean[Vertex];
+        int startI = v.indexOf(s);
+        DFSHelper(startI, visited);
+        System.out.println();
+    }
+
+    private void DFSHelper(int st, boolean[] visited) {
+        visited[st] = true;
+        System.out.print(v.get(st) + " ");
+        for (int i = 0; i < Vertex; i++) {
+            if (Arr[st][i] && !visited[i]) {
+                visited[i] = true;
+                DFSHelper(i, visited);
+            }
+        }
     }
 }
 
 public class Graph_DFS {
-   public static void main(String[] args) {
-    System.out.println("Enter the Vertex: ");
-    Scanner input=new Scanner(System.in);
-    int Vertex=input.nextInt();
-    Graph ob=new Graph(Vertex);
-//Add Vertex
-    ob.addVertex('S');
-    ob.addVertex('A');
-    ob.addVertex('B');
-    ob.addVertex('C');
-    ob.addVertex('D');
-    ob.addVertex('E');
-    ob.addVertex('F');
-    ob.addVertex('G');
-//Add Edges
-    ob.addEdge('S','A');
-    ob.addEdge('S','B');
-    ob.addEdge('S','C');
-    ob.addEdge('A','D');
-    ob.addEdge('B','E');
-    ob.addEdge('C','F');
-    ob.addEdge('D','G');
-    ob.addEdge('E','G');
-    ob.addEdge('F','G');
-  
-// DFS Traversel
-System.out.println("DFS Traversal: ");
-   ob.DFS('S');
+    public static void main(String[] args) {
+        System.out.println("Enter the Vertex: ");
+        Scanner input = new Scanner(System.in);
+        int Vertex = input.nextInt();
+        Graph ob = new Graph(Vertex);
+        // Add Vertex
+        ob.addVertex('S');
+        ob.addVertex('A');
+        ob.addVertex('B');
+        ob.addVertex('C');
+        ob.addVertex('D');
+        ob.addVertex('E');
+        ob.addVertex('F');
+        ob.addVertex('G');
+        // Add Edges
+        ob.addEdge('S', 'A');
+        ob.addEdge('S', 'B');
+        ob.addEdge('S', 'C');
+        ob.addEdge('A', 'D');
+        ob.addEdge('B', 'E');
+        ob.addEdge('C', 'F');
+        ob.addEdge('D', 'G');
+        ob.addEdge('E', 'G');
+        ob.addEdge('F', 'G');
 
-   } 
+        // DFS Traversel
+        System.out.println("DFS Traversal: ");
+        ob.DFS('S');
+
+    }
 }
--- Java/Graph And Tree/Prims_Krushkal/Prims.java
+++ Java/Graph And Tree/Prims_Krushkal/Prims.java
@@ -1,7 +1,7 @@
 
 public class Prims {
     public static void main(String[] args) {
-        System.out.println("Hi!!!");
+  
     }
 }
 
